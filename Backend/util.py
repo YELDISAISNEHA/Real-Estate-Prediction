42d5ee01fbafc82066ec34e72740bd31d52e45e9
SAI SNEHA YELDI <110588134+YELDISAISNEHA@users.noreply.github.com>
2025-07-17 21:49:57 +0530

    util.py

--- Backend/util.py
+++ Backend/util.py
@@ -27,12 +27,12 @@ def load_saved_artifacts():
     print("loading saved artifacts...start")
     global __data_columns
     global __locations
-    with open("./artifacts/columns.json", "r") as f:
+    with open("Backend/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
     global __model
-    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
+    with open("Backend/artifacts/banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
 
     print("Loading saved artifacts...Done")
