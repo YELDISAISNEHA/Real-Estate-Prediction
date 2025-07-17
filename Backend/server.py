980ea923609592d8e771df267718ecc41371fb7a
SAI SNEHA YELDI <110588134+YELDISAISNEHA@users.noreply.github.com>
2025-07-17 22:14:25 +0530

    server.py

--- Backend/server.py
+++ Backend/server.py
@@ -25,7 +25,7 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
